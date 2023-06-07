234210cf1e6769123a62dc4f5a1dfcff8b823df6
krishnapal.singh <krishnapal@gmail.com>
2023-06-07 18:38:37 +0530

    Udpate for share data for services.

--- src/app/useService/s-child/s-child.component.ts
+++ src/app/useService/s-child/s-child.component.ts
@@ -4,18 +4,19 @@ import { DataService } from 'src/app/data.service';
 @Component({
   selector: 'app-s-child',
   templateUrl: './s-child.component.html',
-  styleUrls: ['./s-child.component.css']
+  styleUrls: ['./s-child.component.css'],
 })
-export class SChildComponent  implements OnInit{
-  storeMessage:string = ''
+export class SChildComponent implements OnInit {
+  storeMessage: string = '';
 
-  constructor(private dataService : DataService){}
+  constructor(private dataService: DataService) {}
   ngOnInit(): void {
-    this.dataService.cureentMsg.subscribe((res)=>{
-      this.storeMessage = res
-    })
-
-
+    this.dataService.cureentMsg.subscribe((res) => {
+      this.storeMessage = res;
+    });
   }
 
+  submit() {
+    this.dataService.updateMessage(this.storeMessage);
+  }
 }
